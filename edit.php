96a9de00ba94f80ece220e95fb6f21417998c7b7
imad Rahmouni <rahmounimad@gmail.com>
2019-11-17 20:04:56 +0100

    add form validation

--- edit.php
+++ edit.php
@@ -13,11 +13,22 @@ $dbname = 'hirak';
 
 $db = new db($dbhost, $dbuser, $dbpass, $dbname);
 
-if(isset($_GET["id"])){
-    $hero = $db->query('SELECT name,last_name,occupation,wilaya,birthdate,arrested_date,court,released_date,reason,sentence,comment 
+$id = isset($_GET["id"]) ? $_GET["id"]: null;
+
+if( isset($_POST["submit"]) ){
+  $id = $_POST["id"];
+  echo "printed";
+}
+
+
+
+if(isset($id)){
+    $hero = $db->query('SELECT name,last_name,occupation,wilaya,birthdate,arrested_date,court,released_date,reason,sentence,comment,id 
     FROM heroes 
-    WHERE id = ?', $_GET["id"]
+    WHERE id = ?', $id
     )->fetchArray();
+}else{
+  die();
 }
 
 
@@ -33,7 +44,7 @@ $db->close();
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Fotograp &mdash; Colorlib Website Template</title>
+    <title>Hirak Heroes  &mdash;</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -69,38 +80,47 @@ $db->close();
       <div class="row justify-content-center">
         <div class="col-md-7">
           
-
+            <div class="container">
+            <div class="row text-center justify-content-center mb-5">
+                <div class="col-md-7" data-aos="fade-up">
+                    <h2>
+                        <a href="index.php">
+                            Hirak Heroes 
+                        </a> 
+                    </h2>
+                </div>
+            </div>
           <div class="row">
             <div class="col-lg-8 mb-5">
-              <form action="#">
+              <form action="edit.php" method="POST">
                
-
+                <input type="hidden" for="id" name="id" value="<?php echo $hero["id"]; ?>"/>
                 <div class="row form-group">
                   <div class="col-md-6 mb-3 mb-md-0">
                     <label class="text-black" for="fname">First Name *</label>
-                    <input type="text" id="fname" class="form-control" value="<?php echo $hero["name"]; ?>" required>
+                    <input type="text" id="fname" name="fname" name="fname" class="form-control" value="<?php echo $hero["name"]; ?>" required>
                   </div>
                   <div class="col-md-6">
                     <label class="text-black" for="lname">Last Name *</label>
-                    <input type="text" id="lname" class="form-control" value="<?php echo $hero["last_name"]; ?>"  required>
+                    <input type="text" id="lname" name="lname" class="form-control" value="<?php echo $hero["last_name"]; ?>"  required>
                   </div>
                 </div>
 
                 <div class="row form-group">
                 <div class="col-md-6">
                     <label class="text-black" for="occupation">Occupation</label>
-                    <input type="text" id="occupation" class="form-control" value="<?php echo $hero["occupation"]; ?>" >
+                    <input type="text" id="occupation" name="occupation" class="form-control" value="<?php echo $hero["occupation"]; ?>" >
                   </div>
                   <div class="col-md-6">
                     <label class="text-black" for="wilaya">Wilaya *</label>
-                    <input type="text" id="wilaya" class="form-control"  value="<?php echo $hero["wilaya"]; ?>" required>
+                    <input type="text" id="wilaya" name="wilaya" class="form-control"  value="<?php echo $hero["wilaya"]; ?>" required>
                   </div>
                 </div>
 
                 <div class="row form-group">
                 <div class="col-md-6 mb-3 mb-md-0">
                     <label class="text-black" for="birthdate">Birthdate</label>
-                    <input type="date" id="birthdate" class="form-control"  value="<?php echo $hero["birthdate"]; ?>" >
+                    <input type="date" id="birthdate" name="birthdate" class="form-control"  value="<?php echo $hero["birthdate"]; ?>" >
                   </div>
                   <div class="col-md-6 mb-3 mb-md-0">
                     <label class="text-black" for="arrested_date">Arrestation date *</label>
@@ -112,7 +132,7 @@ $db->close();
                 <div class="row form-group">
                   <div class="col-md-6 mb-3 mb-md-0">
                   <label class="text-black" for="court">Court</label> 
-                    <input type="text" id="court" class="form-control" value="<?php echo $hero["court"]; ?>" >
+                    <input type="text" id="court" name="court" class="form-control" value="<?php echo $hero["court"]; ?>" >
 
                   </div>
                   <div class="col-md-6">
@@ -126,7 +146,7 @@ $db->close();
                   
                   <div class="col-md-12">
                     <label class="text-black" for="charges">Charges</label> 
-                    <input type="text" id="charges" class="form-control" value="<?php echo $hero["reason"]; ?>" >
+                    <input type="text" id="charges" name="charges" class="form-control" value="<?php echo $hero["reason"]; ?>" >
                   </div>
                 </div>
 
@@ -134,26 +154,37 @@ $db->close();
                   
                   <div class="col-md-12">
                   <label class="text-black" for="sentence">Sentence</label>
-                    <input type="text" id="sentence" class="form-control" value="<?php echo $hero["sentence"]; ?>" >
+                    <input type="text" id="sentence" name="sentence" class="form-control" value="<?php echo $hero["sentence"]; ?>" >
                   </div>
                 </div>
 
                 <div class="row form-group">
                   <div class="col-md-12">
                     <label class="text-black" for="comments">Comments</label> 
-                    <textarea name="message" id="comments" cols="30" rows="7" class="form-control" placeholder="Any comments here"><?php echo $hero["comment"];?></textarea>
+                    <textarea name="message" id="comments" name="comments" cols="30" rows="7" class="form-control" placeholder="Any comments here"><?php echo $hero["comment"];?></textarea>
                   </div>
                 </div>
 
                 <div class="row form-group">
                   <div class="col-md-12">
-                    <input type="submit" value="Validate" class="btn btn-primary py-2 px-4 text-white">
+                    <input type="submit" name="submit" value="Validate" class="btn btn-primary py-2 px-4 text-white">
                   </div>
                 </div>
               </form>
             </div>
             <div class="col-lg-3 ml-auto">
               <div class="mb-3 bg-white">
+                <?php
+                  $name = $hero["name"];
+                  $last_name = $hero["last_name"];
+                  $filename = strtolower($name."_".$last_name.".jpg");
+                  $image_files = scandir('images/detenus/');
+                  if (in_array($filename, $image_files))
+                    $avatar = "images/detenus/".$filename;
+                  else
+                    $avatar = "images/hero.jpg";
+                ?>
+                <img src="<?php echo $avatar; ?>" alt="" class="img-fluid">
               </div>
               
             </div>
